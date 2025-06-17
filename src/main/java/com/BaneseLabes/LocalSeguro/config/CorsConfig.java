8f87601785af2770c1fef38cda802b3e94cc10fa
p-argolo <paula.reis@sergipetec.org.br>
2025-06-17 11:47:20 -0300

    :recycle: refactor: configuração do cors

--- src/main/java/com/BaneseLabes/LocalSeguro/config/CorsConfig.java
+++ src/main/java/com/BaneseLabes/LocalSeguro/config/CorsConfig.java
@@ -25,7 +25,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000", "https://your-frontend-domain.com") // Add production frontend URL if needed
+                .allowedOrigins("*") // Add production frontend URL if needed
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*")
                 .allowCredentials(true)
