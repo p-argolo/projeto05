e064cb44afd188bf5e8fb1096f6fd2d30c6052ee
p-argolo <paula.reis@sergipetec.org.br>
2025-06-17 12:09:38 -0300

    :recycle: refactor: configuração do cors

--- src/main/java/com/BaneseLabes/LocalSeguro/config/CorsConfig.java
+++ src/main/java/com/BaneseLabes/LocalSeguro/config/CorsConfig.java
@@ -5,27 +5,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 
 
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("http://localhost:3000")
-//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
-//                .allowedHeaders("*")
-//                .allowCredentials(true)
-//                .maxAge(3600); // Cache preflight response for 1 hour
-//        System.out.println("CORS configuration applied for /safetyPlace/**");
-//    }
-//}
-
 @Configuration
 public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("*") // Add production frontend URL if needed
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
                 .allowedHeaders("*")
                 .allowCredentials(true)
@@ -33,3 +19,17 @@ public class CorsConfig implements WebMvcConfigurer {
         System.out.println("CORS configuration applied for /safetyPlace/**");
     }
 }
+
+    //@Configuration
+    //public class CorsConfig implements WebMvcConfigurer {
+    //    @Override
+    //    public void addCorsMappings(CorsRegistry registry) {
+    //        registry.addMapping("/**")
+    //                .allowedOrigins("*") // Add production frontend URL if needed
+    //                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT")
+    //                .allowedHeaders("*")
+    //                .allowCredentials(true)
+    //                .maxAge(3600); // Cache preflight response for 1 hour
+    //        System.out.println("CORS configuration applied for /safetyPlace/**");
+    //    }
+    //}
